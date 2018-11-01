7649d80555dedf964288edc8f300f88f7a2d2204
Daniel Dauksevic <danieldauk@gmail.com>
2018-11-01 18:27:06 +0200

    add edge case error handling to users post route

--- backend/routes/users/usersPost.js
+++ backend/routes/users/usersPost.js
@@ -5,7 +5,9 @@ const { validateUser, User } = require('../../model/users');
 module.exports = async (req, res) => {
   const validationResult = validateUser(req.body);
   if (validationResult.error) {
-    res.status(400).json(validationResult.error.details);
+    res.status(400).json({
+      error: validationResult.error.details[0].message,
+    });
     return;
   }
 
@@ -17,10 +19,22 @@ module.exports = async (req, res) => {
   });
 
   try {
+    // check if user exists in database
+    const doesUserExist = !!await User.find({ userName }).count();
+    if (doesUserExist) {
+      res.status(400).json({
+        error: 'Given username exists',
+      });
+      return;
+    }
+    // if user does not exist - save in database
     const result = await user.save();
     res.json(result);
   } catch (error) {
+    // if error occurs during saving to db - return error
     winston.error(error);
-    res.status(500).json(error);
+    res.status(500).json({
+      error: error.message,
+    });
   }
 };
