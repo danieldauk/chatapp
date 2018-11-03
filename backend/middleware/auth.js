9b97d35bc7c2cb42453f929e3be19b0b69e4ee27
Daniel Dauksevic <danieldauk@gmail.com>
2018-11-03 20:00:58 +0200

    add authorization middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,27 @@
-// TODO: create authorization middleware
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+const auth = (req, res, next) => {
+  let token = req.header('authorization');
+  if (!token) {
+    res.status(401).json({
+      error: 'Access denied. No token provided.',
+    });
+    return;
+  }
+  try {
+    token = token.replace('Bearer ', '');
+    const certPath = path.join(__dirname, '../id_rsa.pub');
+    const cert = fs.readFileSync(certPath);
+    const payload = jwt.verify(token, cert);
+    req.user = payload;
+    next();
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
+
+module.exports = auth;
