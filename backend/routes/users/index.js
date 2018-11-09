87abdde7f7d4bb711e98b007871d290dca6682bc
Daniel Dauksevic <danieldauk@gmail.com>
2018-11-09 21:25:07 +0200

    add 'users/me' endpoint with 'populate contacts' option.

--- backend/routes/users/index.js
+++ backend/routes/users/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const authMiddleware = require('../../middleware/auth');
 const create = require('./create');
-
+const me = require('./me');
 // nested routes
 const contacts = require('./contacts');
 
@@ -11,5 +12,6 @@ router.use('/:userId/contacts', contacts);
 
 
 router.post('/', create);
+router.get('/me', authMiddleware, me);
 
 module.exports = router;
---
+++ backend/routes/users/me.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+const { User } = require('../../model/users');
+
+module.exports = async (req, res) => {
+  let user;
+  try {
+    if (req.query.contacts === 'true') {
+      user = await User.findOne({ _id: req.user._id }, { password: 0 }).populate('contacts');
+    } else {
+      user = await User.findOne({ _id: req.user._id }, { password: 0 });
+    }
+    res.json(user);
+  } catch (error) {
+    // if error occurs during saving to db - return error
+    winston.error(error);
+    res.status(500).json({
+      error: error.message
+    });
+  }
+};
