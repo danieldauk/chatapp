e106447918410d8372c591a9b1e3a49869a74fcb
Daniel Dauksevic <danieldauk@gmail.com>
2018-11-13 20:01:08 +0200

    add socketio-jwt

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,3 +1,15 @@
+const socketioJwt = require('socketio-jwt');
+const fs = require('fs');
+const path = require('path');
+
+const certPath = path.join(__dirname, '../id_rsa.pub');
+const cert = fs.readFileSync(certPath);
+
 module.exports = (io) => {
-  console.log(io);
+  io.sockets.on('connection', socketioJwt.authorize({
+    secret: cert,
+    timeout: 5000
+  })).on('authenticated', (socket) => {
+    console.log('authenticated');
+  });
 };
