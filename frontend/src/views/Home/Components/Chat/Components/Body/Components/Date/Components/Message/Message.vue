2d7fe7694809f8a9ea679e7372b7beae7f3c85ff
Daniel Dauksevic <danieldauk@gmail.com>
2018-12-01 10:53:39 +0200

    change message layout and style

--- frontend/src/views/Home/Components/Chat/Components/Body/Components/Date/Components/Message/Message.vue
+++ frontend/src/views/Home/Components/Chat/Components/Body/Components/Date/Components/Message/Message.vue
@@ -1,22 +1,17 @@
 <template>
-  <div
-    :class="['message', {'message--own': isOwnMessage}]"
-  >
-    <div class="message__info">
-      <div 
-      v-if="!isPreviousMessageOwn"
-      class="message__info__avatar">
-        <img :src="avatar">
-      </div>
-      <div 
-      v-if="!isPreviousMessageOwn"
-      class="message__info__time">
-        {{ time }}
-      </div>
+  <div :class="['message', {'message--own': isOwnMessage}]">
+    <div class="message__avatar">
+      <img v-if="!isPreviousMessageOwn" :src="avatar">
     </div>
-    <div
-      :class="['message__body', {'message__body--own': isOwnMessage}]"
-    >{{ message.content }}</div>
+    <div class="message__container">
+      <div
+        v-if="!isPreviousMessageOwn"
+        :class="['message__container__time', {'message__container__time--own': isOwnMessage}]"
+      >{{ time }}</div>
+      <div
+        :class="['message__container__body', {'message__container__body--own': isOwnMessage}, {'message__container__body--subsequent': isPreviousMessageOwn}]"
+      >{{ message.content }}</div>
+    </div>
   </div>
 </template>
 
@@ -38,12 +33,12 @@ export default {
     },
     avatar() {
       if (this.isOwnMessage) {
-        return this.$store.getters['user/getAvatarLink'];
+        return this.$store.getters["user/getAvatarLink"];
       }
-      return this.$store.getters['contact/getAvatarLink'](this.message.sender);
+      return this.$store.getters["contact/getAvatarLink"](this.message.sender);
     },
     time() {
-      return this.$options.filters.formatDate(this.message.createdAt, 'HH:mm');
+      return this.$options.filters.formatDate(this.message.createdAt, "HH:mm");
     }
   }
 };
@@ -57,64 +52,74 @@ export default {
   &--own {
     flex-direction: row-reverse;
   }
-  &__info {
+  &__avatar {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    color: $color-silver;
+    justify-content: center;
     min-width: 50px;
-    &__avatar {
-      margin-bottom: 5px;
-      img {
-        height: 25px;
-        width: 25px;
-        border-radius: 50%;
-        display: block;
-      }
-    }
-    &__time {
-      font-size: 10px;
+    img {
+      height: 30px;
+      width: 30px;
+      border-radius: 50%;
+      display: block;
     }
   }
 
-  &__body {
-    font-size: 13px;
-    padding: 10px 15px;
+  &__container {
+    display: flex;
+    flex-direction: column;
     max-width: 50%;
-    border-radius: 0px 3px 3px 3px;
-    position: relative;
-    background: $color-white;
-    color: $color-purple-medium;
-    word-break: break-word;
-    &:not(.message__body--own):after {
-      content: '';
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 0;
-      height: 0;
-      border: 5px solid transparent;
-      border-right-color: $color-white;
-      border-left: 0;
-      border-top: 0;
-      margin-left: -5px;
+    &__time {
+      font-size: 10px;
+      color: $color-silver;
+      &--own {
+        text-align: right;
+      }
     }
-    &--own {
-      background: $color-blue-medium;
-      color: $color-white;
-      border-radius: 3px 0px 3px 3px;
-      &:after {
-        content: '';
+    &__body {
+      box-shadow: 0px 2px 5px 1px rgba($color-purple-ultra-dark, 0.2);
+      font-size: 13px;
+      padding: 5px 15px;
+      height: max-content;
+      border-radius: 0px 3px 3px 3px;
+      position: relative;
+      background: $color-white;
+      color: $color-purple-medium;
+      word-break: break-word;
+      &:not(.message__container__body--own):after {
+        content: "";
         position: absolute;
-        right: 0;
+        left: 0;
         top: 0;
         width: 0;
         height: 0;
         border: 5px solid transparent;
-        border-left-color: $color-blue-medium;
-        border-right: 0;
+        border-right-color: $color-white;
+        border-left: 0;
         border-top: 0;
-        margin-right: -5px;
+        margin-left: -5px;
+      }
+      &--own {
+        background: $color-blue-medium;
+        color: $color-white;
+        border-radius: 3px 0px 3px 3px;
+        &:after {
+          content: "";
+          position: absolute;
+          right: 0;
+          top: 0;
+          width: 0;
+          height: 0;
+          border: 5px solid transparent;
+          border-left-color: $color-blue-medium;
+          border-right: 0;
+          border-top: 0;
+          margin-right: -5px;
+        }
+      }
+      &--subsequent {
+        &:after {
+          content: none;
+        }
       }
     }
   }
