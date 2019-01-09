31a38b0f988fe2abeeb0b1be28ba1145193584bd
Daniel Dauksevic <danieldauk@gmail.com>
2019-01-09 20:10:55 +0200

    fix new contact conversation bug

--- frontend/src/services/socket/listeners/contactEventListeners.js
+++ frontend/src/services/socket/listeners/contactEventListeners.js
@@ -5,8 +5,9 @@ export default (socket) => {
   socket.on(SocketEventsEnum.RESPONSE_CONTACTS, (data) => {
     store.dispatch('contact/setAll', data.contacts);
   });
-  socket.on(SocketEventsEnum.RESPONSE_ADD_CONTACT, () => {
-    store.dispatch('contact/load');
+  socket.on(SocketEventsEnum.RESPONSE_ADD_CONTACT, async () => {
+    await store.dispatch('contact/load');
+    store.dispatch('conversation/loadAll');
   });
   socket.on(SocketEventsEnum.RESPONSE_REMOVE_CONTACT, () => {
     store.dispatch('contact/load');
