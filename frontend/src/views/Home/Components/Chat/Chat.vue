119da204d4fe97d785fd2934f3c55bb1e6c88861
Daniel Dauksevic <danieldauk@gmail.com>
2019-01-22 19:37:13 +0200

    add chat placeholder

--- frontend/src/views/Home/Components/Chat/Chat.vue
+++ frontend/src/views/Home/Components/Chat/Chat.vue
@@ -4,11 +4,12 @@
       v-if="doesCurrentConversationExist"
       slot="header"
     />
-    <app-body slot="body" />
+    <app-body v-if="doesCurrentConversationExist" slot="body" />
     <app-footer
       v-if="doesCurrentConversationExist"
       slot="footer"
     />
+    <app-placeholder v-else slot="body" />
   </app-chat>
 </template>
 
@@ -17,13 +18,15 @@ import Chat from './Layout/Chat.vue';
 import Header from './Components/Header/Header.vue';
 import Body from './Components/Body/Body.vue';
 import Footer from './Components/Footer/Footer.vue';
+import Placeholder from './Components/Placeholder/Placeholder.vue';
 
 export default {
   components: {
     appChat: Chat,
     appHeader: Header,
     appBody: Body,
-    appFooter: Footer
+    appFooter: Footer,
+    appPlaceholder: Placeholder
   },
   computed: {
     doesCurrentConversationExist() {
---
+++ frontend/src/views/Home/Components/Chat/Components/Placeholder/Placeholder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="chat-placeholder">
+    <img class="chat-placeholder__image" src="@/assets/logo-placeholder.svg" >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .chat-placeholder {
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    &__image {
+        height: 50%;
+        width: 50%;
+    }
+  }
+</style>
+
+
+
